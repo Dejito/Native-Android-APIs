f310eb80d2bae51f68646c09bb94da9eadea74c6
dejito <Deerealboy@gmail.com>
2025-09-21 05:21:11 +0100

    added BTUiStates

---
+++ app/src/main/java/com/mobile/nativeandroidapis/bluetooth/presentation/viewmodel/BluetoothUiState.kt
@@ -0,0 +1,9 @@
+package com.mobile.nativeandroidapis.bluetooth.presentation.viewmodel
+
+import com.mobile.nativeandroidapis.bluetooth.domain.BluetoothDevice
+
+
+data class BluetoothUiState(
+    val scannedDevices: List<BluetoothDevice> = emptyList(),
+    val pairedDevices: List<BluetoothDevice> = emptyList(),
+)
