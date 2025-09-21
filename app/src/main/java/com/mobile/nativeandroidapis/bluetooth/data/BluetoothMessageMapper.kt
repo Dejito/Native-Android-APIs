61dba44ac27f1f3ed03fdc25338d9b7bdb2f3f25
dejito <Deerealboy@gmail.com>
2025-09-21 08:28:53 +0100

    added BluetoothMessage data and mapper class

---
+++ app/src/main/java/com/mobile/nativeandroidapis/bluetooth/data/BluetoothMessageMapper.kt
@@ -0,0 +1,18 @@
+package com.mobile.nativeandroidapis.bluetooth.data
+
+import com.mobile.nativeandroidapis.bluetooth.domain.BluetoothMessage
+
+
+fun String.toBluetoothMessage(isFromLocalUser: Boolean): BluetoothMessage {
+    val name = substringBeforeLast("#")
+    val message = substringAfter("#")
+    return BluetoothMessage(
+        message = message,
+        senderName = name,
+        isFromLocalUser = isFromLocalUser
+    )
+}
+
+fun BluetoothMessage.toByteArray(): ByteArray {
+    return "$senderName#$message".encodeToByteArray()
+}
