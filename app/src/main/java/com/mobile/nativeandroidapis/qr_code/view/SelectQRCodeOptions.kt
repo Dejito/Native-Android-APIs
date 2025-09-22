6c0bfd36072d9b89eeb6b439f0d5643ba5519c27
dejito <Deerealboy@gmail.com>
2025-09-22 04:52:52 +0100

    implementing qrcode selection screen

---
+++ app/src/main/java/com/mobile/nativeandroidapis/qr_code/view/SelectQRCodeOptions.kt
@@ -0,0 +1,35 @@
+package com.mobile.nativeandroidapis.qr_code.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.mobile.nativeandroidapis.router.Navigator
+import com.mobile.nativeandroidapis.ui.screens.AppHomePageCard
+import com.mobile.nativeandroidapis.ui.screens.CustomAppBar
+import com.mobile.nativeandroidapis.utils.DisabledBackPress
+
+
+@Composable
+fun SelectQRCodeOption(navigator: Navigator) {
+    Scaffold(
+        topBar = { CustomAppBar(title = "Select Option", onClick = { navigator.navigateBack() }
+        ) }
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            AppHomePageCard(title = "Bluetooth", onClick = { navigator.navToBluetooth() })
+            AppHomePageCard(title = "QRCode", onClick = { navigator.navToQRCodeScanner() })
+            AppHomePageCard(title = "SQLite Cypher", onClick = {})
+            AppHomePageCard(title = "NFC", onClick = {})
+        }
+    }
+}
