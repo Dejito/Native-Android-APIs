5bfea4211c6895ba9157cbe24090a27323ae7086
dejito <Deerealboy@gmail.com>
2025-09-22 03:20:13 +0100

    QRCode scanner screen implementation started

---
+++ app/src/main/java/com/mobile/nativeandroidapis/qr_code/QRScannerScreen.kt
@@ -0,0 +1,110 @@
+package com.mobile.nativeandroidapis.qr_code
+
+import android.Manifest
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mobile.nativeandroidapis.router.Navigator
+import com.mobile.nativeandroidapis.ui.screens.CustomAppBar
+import java.util.regex.Pattern
+
+@Composable
+fun QRCodeScannerScreen(navigator: Navigator, ) {
+    var statusText by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    PermissionRequestDialog(
+        permission = Manifest.permission.CAMERA,
+        onResult = { isGranted ->
+            statusText = if (isGranted) {
+                "Scan QR code now!"
+            } else {
+                "No camera permission!"
+            }
+        },
+    )
+    Scaffold(
+        topBar = {
+            CustomAppBar(title = "Scan QR Code", textColor = Color.Black, backgroundColor = Color.White,
+                onClick = {navigator.goBack()}, tint = Color.Black
+            )
+        },
+    ) {
+            paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+//                .paint(painter = painterResource(id = R.drawable.kegow_logo), alpha = 1f,)
+//                .padding(horizontal = 10.dp)
+                .padding(paddingValues)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleText(text = "Hold your camera over a QR Code to scan",
+                color = Color.Black, fontSize = 13, fontWeight = FontWeight.W500,
+            )
+            TitleText(text = "Hold your camera over a QR Code to scan \n" + "QR Code to scan",
+                color = Color.White, fontSize = 13, fontWeight = FontWeight.W500,
+            )
+            Box{
+                CameraPreview(
+                    modifier = Modifier,
+                    accountDetails = { details ->
+                        try {
+                            if (
+                                Pattern.matches(".*30000.*", details) || Pattern.matches(".*101000.*", details)
+                            ) {
+                                kegowViewModel.setQrCodeData(details)
+                                navigator.navToQRCodeTransfer()
+                            } else {
+                                navigator.navToKegowApp()
+                                context.displayToastMessage("Invalid QR Code. Kindly scan a Kegow QR Code")
+                            }
+                        } catch (e: Exception) {
+//                            navigator.navToKegowApp()
+                        } finally {
+//                            navigator.navToKegowApp()
+                        }
+                    },
+                    onFailed = {
+//                        context.displayToastMessage("Please scan a valid Kegow QR Code")
+//                        navigator.navToKegowApp()
+                    },
+                    navigator = navigator
+                )
+
+                GIFImage(gifImage = R.drawable.scan, modifier = Modifier.size(400.dp))
+            }
+
+            Text(
+                text = buildAnnotatedString {
+                    append("Powered by ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.W500, color = Color(
+                        kegowGreen), fontSize = 14.sp
+                    )) {
+                        append("Runteller ")
+                    }
+                    append("with \u2764")
+                },
+                color = Color.White, fontWeight = FontWeight.W500, fontSize = 12.sp,
+                lineHeight = 2.sp
+            )
+        }
+    }
+}
+
