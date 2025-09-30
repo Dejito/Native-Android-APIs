7c2fe512fd847de2e0601296c1f9e20f6c7ea26a
dejito <Deerealboy@gmail.com>
2025-10-01 00:27:22 +0100

    added WriteNfcTag UseCase implementation

---
+++ app/src/main/java/com/mobile/nativeandroidapis/nfc/domain/usecase/WriteNfcTagUseCase.kt
@@ -0,0 +1,26 @@
+package com.mobile.nativeandroidapis.nfc.domain.usecase
+
+
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import java.nio.charset.Charset
+
+class WriteNfcTagUseCase {
+    fun execute(tag: Tag?, text: String): Boolean {
+        val ndef = Ndef.get(tag) ?: return false
+        val record = NdefRecord.createMime("text/plain", text.toByteArray(Charset.forName("US-ASCII")))
+        val message = NdefMessage(arrayOf(record))
+
+        return try {
+            if (ndef.maxSize < message.toByteArray().size) return false
+            ndef.connect()
+            ndef.writeNdefMessage(message)
+            ndef.close()
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
