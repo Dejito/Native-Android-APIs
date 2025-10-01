c55c302a51669904c9d48948ae5e1b350ff1b5ea
dejito <Deerealboy@gmail.com>
2025-10-01 09:57:07 +0100

    parsing nfc manager class on MainActivity

--- app/src/main/java/com/mobile/nativeandroidapis/MainActivity.kt
+++ app/src/main/java/com/mobile/nativeandroidapis/MainActivity.kt
@@ -1,77 +1,48 @@
 package com.mobile.nativeandroidapis
 
-import android.app.ComponentCaller
-import android.app.PendingIntent
 import android.content.Intent
-import android.content.IntentFilter
-import android.content.pm.PackageManager
-import android.nfc.NdefMessage
-import android.nfc.NfcAdapter
-import android.nfc.Tag
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.ViewModelProvider
+import com.mobile.nativeandroidapis.nfc.data.NfcManager
 import com.mobile.nativeandroidapis.nfc.presentation.view.NfcScreen
 import com.mobile.nativeandroidapis.nfc.presentation.viewmodel.NFCViewModel
 import com.mobile.nativeandroidapis.ui.theme.NativeAndroidAPIsTheme
-import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var nfcViewModel: NFCViewModel
-    private var nfcAdapter: NfcAdapter? = null
-
+    private lateinit var viewModel: NFCViewModel
+    private lateinit var nfcManager: NfcManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        nfcViewModel = ViewModelProvider(this)[NFCViewModel::class.java]
-        val isNFCAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_NFC)
-        nfcViewModel.isNfcAvailable(isNFCAvailable)
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        viewModel = ViewModelProvider(this)[NFCViewModel::class.java]
+        nfcManager = NfcManager(this, viewModel)
+
+        val isNFCAvailable = packageManager.hasSystemFeature(android.content.pm.PackageManager.FEATURE_NFC)
+        viewModel.isNfcAvailable(isNFCAvailable)
+
         setContent {
             NativeAndroidAPIsTheme {
                 NfcScreen()
-//                AppNavigator()
-            }
-        }
-    }
-
-
-    override fun onNewIntent(intent: Intent, caller: ComponentCaller) {
-        super.onNewIntent(intent)
-
-        val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
-        if (intent.action == NfcAdapter.ACTION_TAG_DISCOVERED) {
-            val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-            val messages = rawMsgs?.map { it as NdefMessage }?.toTypedArray()
-            if (nfcViewModel.state.value.isWriting) {
-                nfcViewModel.writeTag(tag)
-            } else {
-                nfcViewModel.readTag(tag, messages)
             }
         }
     }
 
     override fun onResume() {
         super.onResume()
-        nfcViewModel.updateIsEnabled(nfcAdapter?.isEnabled == true)
-
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0,
-            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
-            PendingIntent.FLAG_MUTABLE
-        )
-        val filters = arrayOf(IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED))
-        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, filters, null)
+        nfcManager.onResume()
     }
-
 
     override fun onPause() {
         super.onPause()
-        nfcAdapter?.disableForegroundDispatch(this)
+        nfcManager.onPause()
     }
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        nfcManager.handleIntent(intent)
+    }
 }
