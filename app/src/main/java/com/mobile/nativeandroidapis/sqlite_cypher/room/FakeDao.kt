9aebc31f4961c9e638a13bb38128517af4345539
dejito <Deerealboy@gmail.com>
2025-10-03 09:29:59 +0100

    added dao implementation

---
+++ app/src/main/java/com/mobile/nativeandroidapis/sqlite_cypher/room/FakeDao.kt
@@ -0,0 +1,17 @@
+package com.mobile.nativeandroidapis.sqlite_cypher.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface FakeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(fakeEntity: FakeEntity)
+
+    @Query("SELECT * FROM FakeEntity")
+    suspend fun getFakeEntities():List<FakeEntity>
+
+}
