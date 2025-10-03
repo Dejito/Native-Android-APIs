149cbcc6df4e3263916d9914e8f844ebf568faf4
dejito <Deerealboy@gmail.com>
2025-10-03 09:47:58 +0100

    implemented SqlCipherViewModel

---
+++ app/src/main/java/com/mobile/nativeandroidapis/sqlite_cypher/presentation/viewmodel/SqlCipherViewModel.kt
@@ -0,0 +1,33 @@
+package com.mobile.nativeandroidapis.sqlite_cypher.presentation.viewmodel
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mobile.nativeandroidapis.sqlite_cypher.room.AppDatabase
+import com.mobile.nativeandroidapis.sqlite_cypher.room.FakeEntity
+import kotlinx.coroutines.launch
+
+class SqlCipherViewModel(private val db: AppDatabase) : ViewModel() {
+
+    private val _items = mutableStateListOf<FakeEntity>()
+    val items: List<FakeEntity> get() = _items
+
+    init {
+        retrieveItems()
+    }
+
+    fun insertItem(name: String) {
+        viewModelScope.launch {
+            db.getFakeDao().insert(FakeEntity(name = name))
+            retrieveItems()
+        }
+    }
+
+    fun retrieveItems() {
+        viewModelScope.launch {
+            val result = db.getFakeDao().getFakeEntities()
+            _items.clear()
+            _items.addAll(result)
+        }
+    }
+}
